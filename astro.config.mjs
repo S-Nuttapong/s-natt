3a37ded9431d42bfb7e25ea5e4eb3f1f4adaca3d
Nuttapong <Nuttapong@nuttapongs-MacBook-Pro.local>
2023-02-14 23:42:20 +0700

    feat: add extra slides

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), mdx(), image()],
 });
---
+++ src/markdown/national-coding-day/lodash-solution-direct.md
@@ -0,0 +1,9 @@
+```typescript
+
+//src/index.js
+
+import pad from 'lodash/pad';
+
+console.log(pad('adc', 8, '_'))
+
+```
