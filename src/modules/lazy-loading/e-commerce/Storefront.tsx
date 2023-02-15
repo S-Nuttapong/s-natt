265b10a5526e9efa736920ea9089184e90916b7d
Nuttapong <Nuttapong@nuttapongs-MacBook-Pro.local>
2023-02-15 21:02:44 +0700

    feat: added slide 7 and 8

---
+++ src/modules/lazy-loading/e-commerce/Storefront.tsx
@@ -0,0 +1,7 @@
+export const Storefront = () => {
+  return (
+    <div className="bg-transparent border-2 border-white">
+      <div className=" text-white">this is storefront</div>
+    </div>
+  );
+};
