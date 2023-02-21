cb515edecde571adace762cf8b0ae9009971e01c
Nuttapong <Nuttapong@nuttapongs-MacBook-Pro.local>
2023-02-21 12:08:57 +0700

    chore: 12, 11 parse and load

---
+++ src/modules/national-coding-day/e-commerce/YouMayLikeSection.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+
+type Props = {
+  children?: ReactNode;
+};
+
+export const YouMayLikeSection = ({ children = null }: Props) => (
+  <div className="w-full h-130 flex flex-col p-4 border-2 border-white">
+    <div className="text-white text-3xl">You may also like</div>
+    <div className="flex justify-center">{children}</div>
+  </div>
+);
