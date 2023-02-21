4bbd9565c2cfe40c5e16f802c8f500c403d2fba4
Nuttapong <Nuttapong@nuttapongs-MacBook-Pro.local>
2023-02-21 17:27:53 +0700

    feat: added insturction on lazy load sections

--- src/modules/national-coding-day/e-commerce/StorefrontContainer.tsx
+++ src/modules/national-coding-day/e-commerce/StorefrontContainer.tsx
@@ -3,20 +3,26 @@ import { HowItWorks } from "./HowItWorks";
 
 type Props = {
   children?: ReactNode;
-  instructions?: string[];
+  instruction: {
+    title: string;
+    steps: string[];
+  };
 };
 
-export const StorefrontContainer = ({ children, instructions = [] }: Props) => (
+export const StorefrontContainer = ({ children, instruction }: Props) => (
   <div className="flex flex-col w-full h-full justify-center items-center px-10 py-5 overflow-hidden">
     <div className="flex w-full justify-end z-50">
       <HowItWorks>
-        <ol className="text-white text-sm list-auto list-inside">
-          {instructions.map((instruction) => (
-            <li key={instruction} className="text-inherit">
-              {instruction}
-            </li>
-          ))}
-        </ol>
+        <div className="text-white text-sm">
+          <div className="text-inherit">{instruction.title}</div>
+          <ol className="list-auto list-inside pl-2">
+            {instruction.steps.map((step) => (
+              <li key={step} className="text-inherit">
+                {step}
+              </li>
+            ))}
+          </ol>
+        </div>
       </HowItWorks>
     </div>
     <div className="bg-transparent w-full h-full border-2 border-gray-100 xxl:px-20 xs:px-10 py-10 relative overflow-y-hidden">
